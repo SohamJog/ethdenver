a29a332c55aa91395a21075c7ec185db593bc5b0
Soham Jog <jogsoham2003@gmail.com>
2024-03-01 18:00:01 -0700

    maybe woriing

--- src/GetFunds.js
+++ src/GetFunds.js
@@ -15,48 +15,46 @@ function GetFunds() {
   const [vaultAddress, setVaultAddress] = useState('');
   const { sdk, connected, connecting, provider, chainId } = useSDK();
 
+
+  const fetchVaultAddress = async () => {
+      
+
+    try {
+      console.log("here")
+      const provider = new ethers.BrowserProvider(window.ethereum)
+
+      // MetaMask requires requesting permission to connect users accounts
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+      
+      // The MetaMask plugin also allows signing transactions to
+      // send ether and pay to change state within the blockchain.
+      // For this, you need the account signer...
+      const signer = await provider.getSigner()
+      console.log('Signer:', signer);
+
+
+
+      // Instantiate VaultFactory contract
+      const vaultFactory = new ethers.Contract(VaultFactoryContractAddress, VaultFactoryContractABI, signer);
+
+      // Call the contract's method to get the vault address
+      const fetchedVaultAddress = await vaultFactory.vaults(id);
+      setVaultAddress(fetchedVaultAddress);
+      console.log(fetchedVaultAddress)
+
+      const vault = new ethers.Contract(fetchedVaultAddress, VaultContractABI, signer);
+      await vault.withdraw();
+      setFetched(true);
+    } catch (error) {
+      console.error('Error fetching vault address:', error);
+    }
+  };
   useEffect(() => {
 
-
-    const fetchVaultAddress = async () => {
-      
-
-          try {
-            console.log("here")
-            const provider = new ethers.BrowserProvider(window.ethereum)
-    
-            // MetaMask requires requesting permission to connect users accounts
-            await window.ethereum.request({ method: 'eth_requestAccounts' });
-      
-            
-            // The MetaMask plugin also allows signing transactions to
-            // send ether and pay to change state within the blockchain.
-            // For this, you need the account signer...
-            const signer = await provider.getSigner()
-            console.log('Signer:', signer);
-      
-      
-      
-            // Instantiate VaultFactory contract
-            const vaultFactory = new ethers.Contract(VaultFactoryContractAddress, VaultFactoryContractABI, signer);
-    
-            // Call the contract's method to get the vault address
-            const fetchedVaultAddress = await vaultFactory.vaults(id);
-            setVaultAddress(fetchedVaultAddress);
-            console.log(fetchedVaultAddress)
-    
-            const vault = new ethers.Contract(fetchedVaultAddress, VaultContractABI, signer);
-            await vault.withdraw();
-            setFetched(true);
-          } catch (error) {
-            console.error('Error fetching vault address:', error);
-          }
-        };
-       
-
-    if (id && !fetched) {
-      fetchVaultAddress();
-    }
+    // if (id && !fetched) {
+    //   fetchVaultAddress();
+    // }
   }, [id, fetched, connected]);
 
   if (!connected) {
@@ -64,18 +62,12 @@ function GetFunds() {
       <div>NOT CONNECTED</div>
     )
   }
-  if (!fetched) {
-    return (
-      <div>
-        <h2>Get Funds Page</h2>
-        <p>Loading...</p>
-      </div>
-    )
-  }
+  
 
   return (
     <div>
       <h2>Get Funds Page</h2>
+      <button className='bg-blue rounded' onClick={fetchVaultAddress}> Get Money</button>
       <p>ID: {id}</p>
       
     </div>
